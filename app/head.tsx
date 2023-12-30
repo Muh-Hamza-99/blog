4ffdc3a843c84c583887320b48fecac49ce161a5
Muhammad Hamza <codingtimelapseyt@gmail.com>
2023-12-31 02:30:06 +0300

    Edit AdSense script

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>Hamza's Blog</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7175318304011318" crossorigin="anonymous"></script>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7175318304011318" cross-origin="anonymous"></script>
       <link rel="icon" href="/favicon.ico" />
     </>
   );
