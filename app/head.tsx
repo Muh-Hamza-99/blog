0cd3dc90354fea8d8d400ad31136bca843e244cf
Muhammad Hamza <codingtimelapseyt@gmail.com>
2023-12-31 17:16:09 +0300

    Edit AdSense link

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,7 @@ export default function Head() {
       <title>Hamza's Blog</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="google-adsense-account" content="ca-pub-7175318304011318" />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7175318304011318" crossOrigin="anonymous"></script>
       <link rel="icon" href="/favicon.ico" />
     </>
   );
